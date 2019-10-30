3046e0550bf4e39e9891f6525ee60c2f0e9ad054
soumyad31 <48352690+soumyad31@users.noreply.github.com>
2019-10-30 22:52:01 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-   int a, b, c;
+   int a, b;
    
    printf("Enter two numbers to add\n");
    scanf("%d%d", &a, &b);
    
-   c = a + b;
+  int  c = a + b;
    
    printf("Sum of the numbers = %d\n", c);
    
